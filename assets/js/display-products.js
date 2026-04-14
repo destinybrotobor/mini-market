eae3666947ddb838ab94366a28ba5a9790c4fa59
destinybrotobor <destinybrotos@programmerscity.com>
2026-04-14 18:01:14 +0100

    Added Flash Sales

---
+++ assets/js/display-products.js
@@ -0,0 +1,37 @@
+let displayFeaturedProduct = (products) => {
+    let featureProduct = products.filter((product) => product.isFeatured == true)
+    let productCard = '';
+
+    featureProduct.forEach((productData, index) => {
+        console.log(productData);
+
+        if (index < 6) {
+            productCard += `<div class="min-w-40 relative">
+            <div class="w-full bg-slate-200 h-32 animate-pulse">
+                <img src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/83/7520914/1.jpg?4830" alt="${productData.name}" class="object-cover" />
+            </div>
+            <div class="flex flex-col p-1">
+                <h4>${productData.name}</h4>
+                <span class="font-semibold">₦ ${productData.price}</span>
+                <span class="line-through text-gray-500">₦ ${productData.originalPrice}</span>
+                <span>${productData.stock} items left</span>
+                <div class="w-35 lg:w-40 bg-gray-200 h-2 rounded-full mt-4">
+                    <div class="bg-red-600 h-2 rounded-full w-1/5"></div>
+                </div>
+            </div>
+            <div class="absolute top-0 left-25.25">
+                <span class="bg-amber-300 text-amber-800 rounded-[5px] px-2.5">-${productData.discountPercent}%</span>
+            </div>
+        </div>`;
+        }
+
+        document.getElementById('featured-product-element').innerHTML = productCard;
+    });
+}
+
+// Fetching Products
+fetch('../assets/store/products.json')
+    .then((response) => response.json())
+    .then((data) => {
+        displayFeaturedProduct(data.products);
+    })
